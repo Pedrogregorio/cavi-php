d069eb2be49e8bbd770aa985a8f202b344738b7e
Aluno <aluno@aluno.com>
2019-04-05 12:05:51 -0300

    Login

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -9,9 +9,9 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/Login',
+      name: 'Login',
+      component: Login 
     },
     {
       path: '/about',
