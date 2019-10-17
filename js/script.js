2fccd995fffcff12f69e2072f92ed2c1a632ab3b
Pedrogregorio <pedrogregoriodias675@gmail.com>
2019-10-16 21:04:22 -0300

    Atualização Pedro

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}*/
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
