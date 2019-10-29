0fc24b55385510498bfa5f32ff4b7fafd6888ea5
Pedrogregorio <pedrogregoriodias675@gmail.com>
2019-10-29 20:56:59 -0300

    Atualização Pedro

--- BD/admin.php
+++ BD/admin.php
@@ -5,4 +5,11 @@
 ?>
 
 <h2><?php echo $_SESSION['email'];  ?></h2>
-<h2><a href="logout.php">sair</a></h2>
+<h2><a href="logout.php">sair</a></h2> 
+
+<form method="POST" action="uploadImagens.php" enctype="multipart/form-data">
+	
+	Select Img:<input type="file" name="arquivo">
+	<input type="submit" name="Cadastrar">
+
+</form>
