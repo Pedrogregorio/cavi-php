bd69c819646dfab5b668f207aa7ee28688af891d
Pedro Gregorio <pedrogregorio@jeraapps.com>
2022-05-11 08:14:50 -0400

    utilizar cdn do uikit

--- head.php
+++ head.php
@@ -1,13 +1,11 @@
 <meta charset="utf-8"> 
 
 <title>..::C A V I::..</title>
-
-<!-- teste do uikit -->
-<link rel="stylesheet" type="text/css" href="includes/uikit/css/uikit.min.css">
-<!-- fim teste -->
-<link rel="stylesheet" type="text/css" href="includes/uikit/css/addon-uikit.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.css">
 <link rel="icon" href="IMAGENS/icon.png" type="image/x-icon"/>
 
 <meta name="viewport" content="width=device-width, user-scalable=no">
-<script src="dist/uikit.min.js"></script>
-<script src="dist/uikit-icons.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>
--- includes/processos_php/head.php
+++ includes/processos_php/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8"> 
 <link rel="stylesheet" type="text/css" href="../css/estilo.css">
 <link rel="stylesheet" type="text/css" href="../css/estilo_login.css">
-<link rel="stylesheet" type="text/css" href="../uikit/css/uikit.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.css">
 <link rel="icon" href="../IMAGENS/icon.png" type="image/x-icon"/>
 
 <meta name="viewport" content="width=device-width, user-scalable=no">	
